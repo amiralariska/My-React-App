312d35430927ec1aa5676476e49696a99f677d2d
Amiral Ariska <ariskaamiral@gmail.com>
2022-11-12 15:13:10 +0800

    My React App Updates: - Add Banner - Add Icon to Banner - Add Text And Date to Banner - Add Github Link to Banner - Update Web

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Banner from './components/Banner';
 import Logo from './react-icon.png';
 
 export default function App() {
@@ -8,11 +9,14 @@ export default function App() {
   }
   return (
     <>
-      <img src={Logo} alt='react logo' />
-      <h1>React</h1>
-      <p>Learn React To Create React App</p>
-      <button onClick={tryit}>Click It</button><br /><br />
-      <a href='https://reactjs.org/' target='_blank' rel='noopener noreferrer'>Learn React</a>
+      <Banner />
+      <div className='main'>
+        <img src={Logo} alt='react logo' />
+        <h1>React</h1>
+        <p>Learn React To Create React App</p>
+        <button className='react-btn' onClick={tryit}>Click It</button><br /><br />
+        <a className='react-link' href='https://reactjs.org/' target='_blank' rel='noopener noreferrer'>Learn React</a>
+      </div>
     </>
   );
 }
